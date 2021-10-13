b8ea63d9f279d7388b3c19f30b609ad1031f421e
Tyler Garlick <136096+TylerGarlick@users.noreply.github.com>
2021-10-13 07:33:07 +0000

    Fixed css

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import styled from "@emotion/styled"
 const AppContainer = styled.section`
   display: flex;
   flex-direction: column;
-  border: 1px solid red;
   flex: 1;
 `
 
 export const App = () => (
   <AppContainer>
-    <section>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </section>
+    <h1>Hello, World</h1>
+    <p>This is a test</p>
+    <h2>Start editing to see some magic happen!</h2>
   </AppContainer>
 )
